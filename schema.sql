3e81434d3c8c8ed8f9d728c75e72276bd90d3901
Raul <raulfellipedomingos@gmail.com>
2026-06-05 02:46:25 -0300

    docs: adiciona o schema em SQL do banco de dados

---
+++ schema.sql
@@ -0,0 +1,20 @@
+#Tabela de criação de usuários
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+#Tabela de criação de obras associadas aos usuários
+CREATE TABLE IF NOT EXISTS obras (
+    id SERIAL PRIMARY KEY,
+    usuario_id INTEGER REFERENCES usuarios(id) ON DELETE CASCADE,
+    titulo VARCHAR(255) NOT NULL,
+    categoria VARCHAR(50) NOT NULL, -- Ex: Filme, Jogo (tipo LoL), Anime, Série
+    status VARCHAR(50) NOT NULL,    -- Ex: Concluído, Jogando, Quero Assistir
+    nota INTEGER,                   -- Ex: de 1 a 10
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+#A tabela de integração e unificação das duas obviamente foi ocultada.
